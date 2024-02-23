ae946c0f947cb80fcfbd734ac9a96b0f11133b37
MUTHU RAMAN S <127648597+muthuraman0508@users.noreply.github.com>
2024-02-23 17:37:46 +0530

    Add files via upload

---
+++ and_gate_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+module and_gate_tb;
+	reg a;
+	reg b;
+	wire y;
+	and_gate uut (.a(a),.b(b),.y(y));
+initial begin
+        a = 0; b = 0;
+        #10; 
+		  $display("a = %b, b = %b, y = %b", a, b, y);
+		  a = 0; b = 1;
+        #10; 
+        $display("a = %b, b = %b, y = %b", a, b, y);
+        a = 1; b = 0;
+        #10;
+        $display("a = %b, b = %b, y = %b", a, b, y);
+        a = 1; b = 1;
+        #10; 
+        $display("a = %b, b = %b, y = %b", a, b, y);
+        $finish;
+
+	end
+      
+endmodule
+
